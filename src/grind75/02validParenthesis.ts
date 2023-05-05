1b4801a8731753c47c808336150078bd48b790a6
Arthur Oliveira <arthur.oliveira@birdie.ai>
2023-05-05 16:27:06 -0300

    fix valid parenthesis code

--- src/grind75/02validParenthesis.ts
+++ src/grind75/02validParenthesis.ts
@@ -1,25 +1,41 @@
-const isValid = (word: string): boolean => {
-  const stack = word.split('')
-
-  const brackets = {
-    ')':'(' ,
-    '}': '{',
-    ']':'['
-  }
-
-
-  while(stack.length > 0) {
-    const firstBracket = stack.shift()
-    const secondBracket = stack.shift()
-
-    if(brackets[secondBracket as keyof typeof brackets] !== firstBracket) {
-      return false
-    }
-  }
-
-  return true
+interface IBrackets {
+  '(': string
+  '{': string
+  '[': string
 }
 
+function isValid(word: string): boolean {
+  if(word.trim() === '') return true
+  
+  const brackets: IBrackets = {
+    '(': ')',
+    '{': '}',
+    '[':']'
+  }
+
+
+  const stack = []
+  for(let char of word) {
+    if(char in brackets) {
+      stack.push(brackets[char as keyof IBrackets])
+      continue
+    }
+
+    if(!stack.length) return false
+
+    if(stack[stack.length - 1] === char) stack.pop()
+  }
+
+  return !stack.length
+}
+
+console.log(isValid(''))
 console.log(isValid('()'))
 console.log(isValid('()[]{}'))
 console.log(isValid('(]'))
+console.log(isValid('({[]})'))
+
+
+// n => length of string
+// time complexity O(n)
+// space complexity O(n)
